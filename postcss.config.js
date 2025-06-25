06d5c9617573fdeb8401230904454c3a7fdf03dd
sanjay.cs22@bitsathy.ac.in <sanjay.cs22@bitsathy.ac.in>
2025-06-25 14:52:10 +0530

    Fix: Add tailwindcss and valid Node.js version

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,10 @@
+// postcss.config.js or .cjs
+import tailwind from '@tailwindcss/postcss'
+import autoprefixer from 'autoprefixer'
+
 export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
+  plugins: [
+    tailwind(),
+    autoprefixer()
+  ],
+}
